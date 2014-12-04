8db311d35f1962f87fda9f8c33356078023b2f0c
owner <owner@ikeda-PC>
2014-12-04 23:34:57 +0900

    こみっとん

--- src/com/example/propetyanimation/MainActivity.java
+++ src/com/example/propetyanimation/MainActivity.java
@@ -12,15 +12,12 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.ImageView;
+import android.widget.Button;
 
 public class MainActivity extends Activity implements View.OnClickListener {
 
-	float degree;
-	float distance;
-
-	ImageView target = (ImageView)findViewById(R.id.target);
-
+	float degree = 45;
+	float distance = -200;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -34,12 +31,11 @@ public class MainActivity extends Activity implements View.OnClickListener {
 		super.onResume();
 
 
-		//ImageView target = (ImageView)findViewById(R.id.target);
+		Button target = (Button)findViewById(R.id.target);
 		target.setOnClickListener(this);
 
 
-		degree = 45;
-		distance = -200;
+
 
 		// AnimatorSetに渡すAnimatorのリストです
 	    List<Animator> animatorList= new ArrayList<Animator>();
@@ -78,53 +74,27 @@ public class MainActivity extends Activity implements View.OnClickListener {
 	    animatorSet.start();
 	}
 
+
 	@Override
 	public void onClick(View v) {
 		switch(v.getId()) {
 		case R.id.target:
 
-			degree = 45;
-			distance = 200;
+			Button target = (Button)findViewById(R.id.target);
 
-			// AnimatorSetに渡すAnimatorのリストです
-		    List<Animator> animatorList= new ArrayList<Animator>();
+			// rotationプロパティを0fから360fに変化させます
+		    ObjectAnimator objectAnimator = ObjectAnimator.ofFloat( target, "rotation", 0f, 360f );
 
-		    // alphaプロパティを0fから1fに変化させます
-		    ObjectAnimator alphaAnimator = ObjectAnimator.ofFloat( target, "alpha", 0f, 1f );
-		    // 2秒かけて実行させます
-		    alphaAnimator.setDuration( 2000 );
-		    // リストに追加します
-		    animatorList.add( alphaAnimator );
-
-		    // 距離と半径から到達点となるX座標、Y座標を求めます
-		    float toX = (float) ( distance * Math.cos( Math.toRadians( degree ) ) );
-		    float toY = (float) ( distance * Math.sin( Math.toRadians( degree ) ) );
-
-		    // translationXプロパティを0fからtoXに変化させます
-		    PropertyValuesHolder holderX = PropertyValuesHolder.ofFloat( "translationX", 0f, toX );
-		    // translationYプロパティを0fからtoYに変化させます
-		    PropertyValuesHolder holderY = PropertyValuesHolder.ofFloat( "translationY", 0f, toY );
-		    // rotationプロパティを0fから360に変化させます
-		    PropertyValuesHolder holderRotaion = PropertyValuesHolder.ofFloat( "rotation", 0f, 360f );
-
-		    // targetに対してholderX, holderY, holderRotationを同時に実行します
-		    ObjectAnimator translationXYAnimator =
-		            ObjectAnimator.ofPropertyValuesHolder( target, holderX, holderY, holderRotaion );
-		    // 2秒かけて実行させます
-		    translationXYAnimator.setDuration( 2000 );
-		    // リストに追加します
-		    animatorList.add( translationXYAnimator );
-
-		    final AnimatorSet animatorSet = new AnimatorSet();
-		    // リストのAnimatorを順番に実行します
-		    animatorSet.playSequentially( animatorList );
+		    // 3秒かけて実行させます
+		    objectAnimator.setDuration( 3000 );
 
 		    // アニメーションを開始します
-		    animatorSet.start();
+		    objectAnimator.start();
 
 			break;
 		}
 	}
+
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
@@ -145,35 +115,6 @@ public class MainActivity extends Activity implements View.OnClickListener {
 		return super.onOptionsItemSelected(item);
 	}
 
-	/**
-	 * 2秒かけて引数に与えた角度と距離の位置に回転させながらターゲットを移動させる
-	 *
-	 * @param target
-	 * @param degree
-	 * @param distance
-	 */
-	public void animatePropertyValuesHolderSample( ImageView target, float degree, float distance ) {
 
 
-	    // 距離と角度から到達点となるX座標、Y座標を求めます
-	    float toX = (float) ( distance * Math.cos( Math.toRadians( degree ) ) );
-	    float toY = (float) ( distance * Math.sin( Math.toRadians( degree ) ) );
-
-	    // translationXプロパティを0fからtoXに変化させます
-	    PropertyValuesHolder holderX = PropertyValuesHolder.ofFloat( "translationX", 0f, toX );
-	    // translationYプロパティを0fからtoYに変化させます
-	    PropertyValuesHolder holderY = PropertyValuesHolder.ofFloat( "translationY", 0f, toY );
-	    // rotationプロパティを0fから360fに変化させます
-	    PropertyValuesHolder holderRotaion = PropertyValuesHolder.ofFloat( "rotation", 0f, 360f );
-
-	    // targetに対してholderX, holderY, holderRotationを同時に実行させます
-	    ObjectAnimator objectAnimator = ObjectAnimator.ofPropertyValuesHolder(
-	            target, holderX, holderY, holderRotaion );
-
-	    // 2秒かけて実行させます
-	    objectAnimator.setDuration( 2000 );
-
-	    // アニメーションを開始します
-	    objectAnimator.start();
-	}
 }
